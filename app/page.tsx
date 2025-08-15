ab5a68c082e58d218968500172ecd4aaa5fd21bf
ArunSingh1 <arunsinghmailid@gmail.com>
2025-08-15 22:14:56 +0530

    modifed page.tsx file to check deployment in custom domain

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Check the vercel deployment and custom domain works.
           </li>
         </ol>
 
